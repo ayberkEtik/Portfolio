aef6d257338ff5ab0f0fca99a84b9f33afe395fb
Ayberk Etik <95998730+ayberkEtik@users.noreply.github.com>
2024-08-10 17:41:06 +0300

    balıkcı.py

--- balıkcı.py
+++ balıkcı.py
@@ -1,8 +1,14 @@
+#This Code for simple clicking
+
+#İmporting package
 import time
 import pyautogui as pg
 import keyboard
 
+#Time between clicks
+sleepTime = 7.6
+
 while keyboard.is_pressed('q') != True:
-    time.sleep(7.6)
+    time.sleep(sleepTime)
     pg.leftClick()
     pg.leftClick()
